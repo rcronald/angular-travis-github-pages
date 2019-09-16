1df3c8c8afa7fca4fae265fa09b92fe8800e824c
Ronald Requena <rrequena@bcp.com.pe>
2019-09-15 23:08:21 -0500

    updating e2e testing suites

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-travis-github-pages!');
+    expect(page.getTitleText()).toEqual('Angular Content Management System');
   });
 
   afterEach(async () => {
